2103d4a739a91be1b1bd4971a3b5bf40a82ea9f9
guillermedev75 <gui.manhaes75@gmail.com>
2024-03-05 12:27:47 -0300

    update setup and git

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml_access_key_extractor',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT License',
     author='Guillerme Rezende Manhaes',
     keywords='nota_fiscal',
-    description=u'Lib to access nf from Jira',
+    description=u'Biblioteca para extrair chave de acesso na nota fiscal em PDF',
     packages=find_packages(),
     package_data={'access_key_extractor': ['*']},
     include_package_data=True,
     install_requires=[
         'pdfplumber',
-        'scikit-learn>=0.24.0'  # Certifique-se de ajustar a versão conforme necessário
+        'scikit-learn>=0.24.0'
     ],
 )
